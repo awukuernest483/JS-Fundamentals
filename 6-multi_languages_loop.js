58c88942e2215c4522b6f2f4556e0069c360720d
ernestjr <awukuernest@gmail.com>
2025-05-24 22:50:35 +0000

    update the JavaScript files for the 0x01-ES6_promise project

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+const arrayList = ["C is fun", "Python is cool", "JavaScript is amazing"]
+
+// for(let x = 0; x < arrayList.length; x++){
+//     console.log(arrayList[x])
+// }
+
+
+
+let x = 0;
+while (x < arrayList.length) {
+    console.log(arrayList[x]);
+    x++;
+}
